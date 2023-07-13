a33d127d3472188a79d4873d7460112f55f82f08
seancaleb <scmubias@gmail.com>
2023-07-13 22:50:56 +0800

    test: add a utility function to allow tests for responsiveness of a component

--- src/lib/test-utils.tsx
+++ src/lib/test-utils.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable react-refresh/only-export-components */
 import { ReactElement, ReactNode } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import ApplicationProviders from "@/components/ApplicationProviders";
 import { BrowserRouter } from "react-router-dom";
+import mediaQuery from "css-mediaquery";
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
@@ -17,3 +19,26 @@ const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">
 
 export * from "@testing-library/react";
 export { customRender as render };
+
+/**
+ * @desc  Used for testing responsiveness
+ */
+export const createMatchMedia = (width: number) => {
+  return (query: string) => {
+    return {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      matches: mediaQuery.match(query, { width }),
+      media: "",
+      addListener: () => {},
+      removeListener: () => {},
+      onchange: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => true,
+    };
+  };
+};
+
+export const resizeScreenSize = (width: number) => {
+  window.matchMedia = createMatchMedia(width);
+};
