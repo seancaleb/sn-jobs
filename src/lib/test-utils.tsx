49743ba97cf3a8918978a2d44500788c4f93c23d
seancaleb <scmubias@gmail.com>
2023-07-13 20:32:50 +0800

    test: add BrowserRouter to Providers

--- src/lib/test-utils.tsx
+++ src/lib/test-utils.tsx
@@ -1,10 +1,19 @@
 /* eslint-disable react-refresh/only-export-components */
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import ApplicationProviders from "@/components/ApplicationProviders";
+import { BrowserRouter } from "react-router-dom";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <ApplicationProviders>
+      <BrowserRouter>{children}</BrowserRouter>
+    </ApplicationProviders>
+  );
+};
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) =>
-  render(ui, { wrapper: ApplicationProviders, ...options });
+  render(ui, { wrapper: Providers, ...options });
 
 export * from "@testing-library/react";
 export { customRender as render };
