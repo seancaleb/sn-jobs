fe446db809f04be1c6b6c73e6f10d04a318ba0b0
seancaleb <scmubias@gmail.com>
2023-07-13 22:53:25 +0800

    refactor: add conditional rendering for navigation items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { Menu } from "lucide-react";
+import { useMediaQuery } from "@mantine/hooks";
 
 const Header = () => {
+  const isDesktop = useMediaQuery("(min-width: 40em)");
+
   return (
     <header
       className="font-helvetica-cyr h-16 border-b border-slate-200 w-full"
@@ -16,22 +20,31 @@ const Header = () => {
           SNJOBS
         </NavLink>
 
-        <nav>
-          <ul className="flex space-x-6">
-            <li>
-              <Button>Post a Job</Button>
-            </li>
-            <li>
-              <Separator orientation="vertical" />
-            </li>
-            <li>
-              <div className="flex space-x-2">
-                <Button variant="ghost">Sign In</Button>
-                <Button variant="ghost">Sign Up</Button>
-              </div>
-            </li>
-          </ul>
-        </nav>
+        {isDesktop ? (
+          <nav>
+            <ul className="flex space-x-6">
+              <li>
+                <Button>Post a Job</Button>
+              </li>
+              <li>
+                <Separator orientation="vertical" />
+              </li>
+              <li>
+                <div className="flex space-x-2">
+                  <Button variant="ghost">Sign In</Button>
+                  <Button variant="ghost">Sign Up</Button>
+                </div>
+              </li>
+            </ul>
+          </nav>
+        ) : (
+          <Menu
+            role="graphics-document"
+            aria-label="navigation menu burger"
+            size={32}
+            className="cursor-pointer"
+          />
+        )}
       </div>
     </header>
   );
