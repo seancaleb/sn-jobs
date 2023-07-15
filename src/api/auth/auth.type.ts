e98f1445383e163d4167058dffb41476be828249
seancaleb <scmubias@gmail.com>
2023-07-16 00:33:14 +0800

    refactor: parse and login user credentials from decoded token

--- src/api/auth/auth.type.ts
+++ src/api/auth/auth.type.ts
@@ -1,3 +1,14 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email(),
+  age: z.number().nullable(),
+  role: z.enum(["admin", "user", "employer"]),
+  exp: z.number(),
+});
+
 export type Token = {
   accessToken: string;
 };
@@ -6,3 +17,5 @@ export type LoginUser = {
   email: string;
   password: string;
 };
+
+export type User = z.infer<typeof userSchema>;
