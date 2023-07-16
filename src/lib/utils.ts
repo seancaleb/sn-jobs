bb54369351b7efceee790782c2bb74b3f2ebb76c
seancaleb <scmubias@gmail.com>
2023-07-16 15:56:04 +0800

    refactor: add toastResponseFormatter util function for formatting response messages

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const toastResponseFormatter = (responseMessage: string) => {
+  const separatorIndex = responseMessage.indexOf(":");
+  const title = responseMessage.substring(0, separatorIndex).trim();
+  const description = responseMessage.substring(separatorIndex + 1).trim();
+
+  return { title, description };
+};
