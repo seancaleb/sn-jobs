857c5d8e7fb85ad1499eb1624aac53749c941d0f
seancaleb <scmubias@gmail.com>
2023-07-16 15:55:04 +0800

    refactor: add Toast component to Root application to listen for toast notifications

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router";
 import Header from "@/components/Header/Header";
 import TopLoadingBar from "@/components/TopLoadingBar";
 import Footer from "@/components/Footer/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const Root = () => {
   return (
@@ -9,6 +10,7 @@ const Root = () => {
       <Header />
       <main className="relative min-h-screen container">
         <TopLoadingBar />
+        <Toaster />
         <Outlet />
         <Footer />
       </main>
