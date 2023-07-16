c6e3ec4faa1ee43b8780c14c704ea05e493a38fb
seancaleb <scmubias@gmail.com>
2023-07-17 03:51:50 +0800

    test: add afterEach() callback to reset screen size back to default for all tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
+import { resizeScreenSize } from "@/lib/test-utils";
 import "@testing-library/jest-dom";
 
 global.matchMedia =
@@ -10,3 +11,12 @@ global.matchMedia =
       removeListener: function () {},
     };
   };
+
+afterEach(() => {
+  const initialWindowSize = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  resizeScreenSize(initialWindowSize.width);
+});
