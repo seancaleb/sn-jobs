a8aee7468eb34c0966ead7543bad7f31215b85a3
seancaleb <scmubias@gmail.com>
2023-07-17 02:48:13 +0800

    feat: add request query to register a user

--- src/api/auth/auth.type.ts
+++ src/api/auth/auth.type.ts
@@ -13,7 +13,19 @@ export type Token = {
   accessToken: string;
 };
 
+export type ResponseMessage = {
+  message: string;
+};
+
 export type LoginUser = {
+  email: string;
+  password: string;
+};
+
+export type RegisterUser = {
+  firstName: string;
+  lastName: string;
+  role: "user" | "employer";
   email: string;
   password: string;
 };
