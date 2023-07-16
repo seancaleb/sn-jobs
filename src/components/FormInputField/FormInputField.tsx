73820e3ee5bca4e9d71562034adde778909cba01
seancaleb <scmubias@gmail.com>
2023-07-17 02:41:43 +0800

    refactor: add the following components
    1. FormInputField
    2. FormSelectField

---
+++ src/components/FormInputField/FormInputField.tsx
@@ -0,0 +1,41 @@
+import { UseFormReturn, FieldValues, Path } from "react-hook-form";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { ComponentProps, ReactNode } from "react";
+
+type FormInputFieldProps<T extends FieldValues> = {
+  control: UseFormReturn<T>["control"];
+  name: Path<T>;
+  label: string;
+  type?: string;
+  placeholder?: string;
+  InputProps?: ComponentProps<typeof Input>;
+  children?: ReactNode;
+};
+
+const FormInputField = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  type = "text",
+  placeholder = "",
+  InputProps,
+  children,
+}: FormInputFieldProps<T>) => (
+  <FormField
+    control={control}
+    name={name}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel>{label}</FormLabel>
+        <FormControl>
+          <Input type={type} placeholder={placeholder} {...field} {...InputProps} />
+        </FormControl>
+        {children}
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export default FormInputField;
