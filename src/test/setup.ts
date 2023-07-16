635080ee675fa11d5373e79c5ae9515a959db223
seancaleb <scmubias@gmail.com>
2023-07-17 05:20:09 +0800

    fix: resolve issue with 'ResizeObserver is not defined' for third party libraries using it

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { resizeScreenSize } from "@/lib/test-utils";
 import "@testing-library/jest-dom";
+import { vi } from "vitest";
 
 global.matchMedia =
   global.matchMedia ||
@@ -12,6 +13,14 @@ global.matchMedia =
     };
   };
 
+//  Resolve issue 'ResizeObserver is not defined' for third party libraries using it
+global.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+//  Reset window size back to default after eact test cases
 afterEach(() => {
   const initialWindowSize = {
     width: window.innerWidth,
