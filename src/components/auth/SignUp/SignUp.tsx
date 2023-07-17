a32c258bc54c8a0e79411e0e51f12d9d0c0f696c
seancaleb <scmubias@gmail.com>
2023-07-17 21:35:25 +0800

    refactor: remove FormDescription import

--- src/components/auth/SignUp/SignUp.tsx
+++ src/components/auth/SignUp/SignUp.tsx
@@ -2,7 +2,7 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
-import { Form, FormDescription } from "@/components/ui/form";
+import { Form } from "@/components/ui/form";
 import { DevTool } from "@hookform/devtools";
 import { Link } from "react-router-dom";
 import { useLoginUser, useRegisterUser } from "@/api/auth/auth";
