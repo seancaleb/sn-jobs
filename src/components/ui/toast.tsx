3cc7bc4dad3b6c40081dabace68ce833228c272f
seancaleb <scmubias@gmail.com>
2023-07-18 01:31:10 +0800

    style: modify destructive variant of Toast component

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed top-0 left-0 sm:left-auto z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
       className
     )}
     {...props}
@@ -28,8 +28,7 @@ const toastVariants = cva(
     variants: {
       variant: {
         default: "bg-background border",
-        destructive:
-          "group destructive border-destructive bg-destructive text-destructive-foreground",
+        destructive: "group bg-red-100 border-red-100 bg-red-100 text-red-900",
       },
     },
     defaultVariants: {
