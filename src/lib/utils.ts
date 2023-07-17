15a2edded9225bb41cf45c1d1dd0b8d77ea4dd11
seancaleb <scmubias@gmail.com>
2023-07-18 05:06:00 +0800

    refactor: add formatJobPostTime utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { toast } from "@/components/ui/use-toast";
+import { isThisWeek, isThisMonth } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -37,4 +38,37 @@ export const displayErrorNotification = (
   const toastId = toastFn({ title, description, variant: "destructive" }).id;
 
   initNotificationId(toastId);
+};
+
+//  Date formatter for a specific job post
+export const formatJobPostTime = (date: Date) => {
+  const now = new Date();
+
+  if (isThisWeek(date)) {
+    const hoursAgo = Math.floor((now.getTime() - date.getTime()) / (1000 * 60 * 60));
+    const daysAgo = Math.floor((now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24));
+
+    if (hoursAgo <= 2) {
+      return "Just now";
+    } else if (daysAgo === 0) {
+      return "Today";
+    } else if (daysAgo === 1) {
+      return "Yesterday";
+    } else {
+      return `${daysAgo} days ago`;
+    }
+  }
+
+  if (isThisMonth(date)) {
+    const daysAgo = Math.floor((now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24));
+    if (daysAgo < 7) {
+      return `${daysAgo} days ago`;
+    } else {
+      const weeksAgo = Math.floor(daysAgo / 7);
+      return `${weeksAgo} week${weeksAgo === 1 ? "" : "s"} ago`;
+    }
+  }
+
+  const monthsAgo = Math.floor((now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24 * 30));
+  return `${monthsAgo + 1} month${monthsAgo === 0 ? "" : "s"} ago`;
 };
