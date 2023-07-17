3e8e7350d2a38faebf05bba95c72f508b2aed9af
seancaleb <scmubias@gmail.com>
2023-07-18 03:04:47 +0800

    refactor: add and listen for toast notifications

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,9 +4,19 @@ import TopLoadingBar from "@/components/TopLoadingBar";
 import Footer from "@/components/Footer/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
+import { useAppSelector } from "./app/hooks";
+import { selectNotification } from "@/features/notification/notificationSlice";
+import { useToast } from "@/components/ui/use-toast";
+import { useEffect } from "react";
 
 const Root = () => {
   const location = useLocation();
+  const { id } = useAppSelector(selectNotification);
+  const { dismiss } = useToast();
+
+  useEffect(() => {
+    if (id) dismiss(id);
+  }, [location.pathname]);
 
   const isSignInRoute = location.pathname === "/sign-in";
   const isSignUpRoute = location.pathname === "/sign-up";
