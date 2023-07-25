cf9e885c0d7a6d692f3f9f69f8725f03b9260102
seancaleb <scmubias@gmail.com>
2023-07-26 04:02:43 +0800

    refactor: pass queryParams.fromAge to JobsFilter as props

--- src/routes/Jobs/Jobs.page.tsx
+++ src/routes/Jobs/Jobs.page.tsx
@@ -111,7 +111,7 @@ const JobsPage = () => {
               </div>
 
               {/* Filters  */}
-              <JobsFilter />
+              <JobsFilter fromAge={queryParams.fromAge} />
             </div>
           )}
 
