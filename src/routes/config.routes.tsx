a4eb1303d7500b485afc80e44ed4605e9fbfc5f3
seancaleb <scmubias@gmail.com>
2023-07-27 04:32:19 +0800

    refactor: remove loader for Profile component

--- src/routes/config.routes.tsx
+++ src/routes/config.routes.tsx
@@ -5,7 +5,7 @@ import Root from "../Root.tsx";
 import Home from "@/routes/Home/Home.tsx";
 import Login from "@/routes/auth/Login/Login.page.tsx";
 import ErrorPage from "@/error-page.tsx";
-import Profile, { loader as profileLoader } from "@/routes/Profile/Profile.page.tsx";
+import Profile from "@/routes/Profile/Profile.page.tsx";
 import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute.tsx";
 import Register from "@/routes/auth/Register/Register.page.tsx";
 import Jobs, { loader as jobsLoader } from "@/routes/Jobs/Jobs.page.tsx";
@@ -32,7 +32,6 @@ export const routesConfig: RouteObject[] = [
       },
       {
         path: ":role/profile",
-        loader: profileLoader,
         element: (
           <ProtectedRoute>
             <Profile />
