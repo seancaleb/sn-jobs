ae9463b6cd5353713400d82ffc7dafda2b3a6cb5
seancaleb <scmubias@gmail.com>
2023-07-28 02:45:25 +0800

    refactor: add RootRouter component to wrap RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ApplicationProviders from "@/components/ApplicationProviders.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { routesConfig } from "@/routes/config.routes";
+import RootRouter from "@/routes/config.routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApplicationProviders>
-      <RouterProvider router={createBrowserRouter(routesConfig)} />
+      <RootRouter />
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </ApplicationProviders>
   </React.StrictMode>
--- src/routes/config.routes.tsx
+++ src/routes/config.routes.tsx
@@ -1,52 +1,61 @@
 import { QueryClient } from "@tanstack/react-query";
-import { type RouteObject } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Root from "../Root.tsx";
 import Home from "@/routes/Home/Home.tsx";
 import Login from "@/routes/auth/Login/Login.page.tsx";
 import ErrorPage from "@/error-page.tsx";
-import Profile from "@/routes/Profile/Profile.page.tsx";
+import Profile, { loader as profileLoader } from "@/routes/Profile/Profile.page.tsx";
 import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute.tsx";
 import Register from "@/routes/auth/Register/Register.page.tsx";
 import Jobs, { loader as jobsLoader } from "@/routes/Jobs/Jobs.page.tsx";
 
 const queryClient = new QueryClient();
 
-export const routesConfig: RouteObject[] = [
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "sign-in",
-        element: <Login />,
-      },
-      {
-        path: "sign-up",
-        element: <Register />,
-      },
-      {
-        path: ":role/profile",
-        element: (
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "jobs",
-        loader: jobsLoader(queryClient),
-        element: (
-          <ProtectedRoute>
-            <Jobs />
-          </ProtectedRoute>
-        ),
-      },
-    ],
-  },
-];
+const RootRouter = () => {
+  return (
+    <RouterProvider
+      router={createBrowserRouter([
+        {
+          path: "/",
+          element: <Root />,
+          errorElement: <ErrorPage />,
+          children: [
+            {
+              index: true,
+              element: <Home />,
+            },
+            {
+              path: "sign-in",
+              element: <Login />,
+            },
+            {
+              path: "sign-up",
+              element: <Register />,
+            },
+            {
+              path: ":role/profile",
+              loader: profileLoader(queryClient),
+              element: (
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              ),
+            },
+            {
+              path: "jobs",
+              loader: jobsLoader(queryClient),
+              element: (
+                <ProtectedRoute>
+                  <Jobs />
+                </ProtectedRoute>
+              ),
+            },
+          ],
+        },
+      ])}
+    />
+  );
+};
+
+export default RootRouter;
