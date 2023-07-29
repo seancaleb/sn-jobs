e151a75904be54a04022d250f62ce2bd2cd967ad
seancaleb <scmubias@gmail.com>
2023-07-29 11:21:27 +0800

    style: minor style changes

--- src/components/UserAccountRoute.tsx
+++ src/components/UserAccountRoute.tsx
@@ -10,31 +10,31 @@ const links = [
   {
     label: "My Profile",
     path: "profile",
-    icon: <ProfileUser className="mr-3 h-5 w-5" />,
+    icon: <ProfileUser className="mr-3 h-5 w-5" strokeWidth={2.5} />,
     role: "generic",
   },
   {
     label: "Applied Jobs",
     path: "applied-jobs",
-    icon: <Briefcase className="mr-3 h-5 w-5" />,
+    icon: <Briefcase className="mr-3 h-5 w-5" strokeWidth={2.5} />,
     role: "user",
   },
   {
     label: "Bookmarked Jobs",
     path: "bookmarked-jobs",
-    icon: <Bookmark className="mr-3 h-5 w-5" />,
+    icon: <Bookmark className="mr-3 h-5 w-5" strokeWidth={2.5} />,
     role: "user",
   },
   {
     label: "My Job Listings",
     path: "job-listings",
-    icon: <ClipboardList className="mr-3 h-5 w-5" />,
+    icon: <ClipboardList className="mr-3 h-5 w-5" strokeWidth={2.5} />,
     role: "employer",
   },
   {
     label: "Privacy & Security",
     path: "privacy-and-security",
-    icon: <Lock className="mr-3 h-5 w-5" />,
+    icon: <Lock className="mr-3 h-5 w-5" strokeWidth={2.5} />,
     role: "generic",
   },
 ];
@@ -43,7 +43,7 @@ const UserAccountRoute = () => {
   const user = useAppSelector(selectUser) as User;
 
   return (
-    <div className="py-12 space-y-4">
+    <div className="py-12 space-y-6">
       <div className="space-y-1">
         <div className="text-2xl tracking-tight font-bold">My Account</div>
         <p className="text-[0.9375rem]">Centralize your profile and settings.</p>
@@ -51,7 +51,7 @@ const UserAccountRoute = () => {
 
       <div className="flex gap-6 items-start">
         <aside className="p-6 rounded-md border border-slate-200 max-w-[14em] w-full">
-          <ul className="space-y-6 text-[0.9375rem] text-light">
+          <ul className="space-y-6 text-[0.9375rem] font-medium">
             {links.map(({ label, path, icon, role }) =>
               role === "generic" ? (
                 <li key={nanoid()}>
