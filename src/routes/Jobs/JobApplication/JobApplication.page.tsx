f85a3aa77f28217e430df2e8eed2e6106f05e6d3
Sean Caleb <scmubias@gmail.com>
2023-08-09 16:58:02 +0800

    refactor: refactor JobApplication page

--- src/routes/Jobs/JobApplication/JobApplication.page.tsx
+++ src/routes/Jobs/JobApplication/JobApplication.page.tsx
@@ -39,9 +39,9 @@ export const loader =
 
 const JobApplication = () => {
   const params = useParams<{ jobId: string }>();
-  const data = useLoaderData() as LoaderReturnType<typeof loader>;
-  const initialJobData = (data as { initialJobData: JobDetails }).initialJobData;
-  const { data: jobData } = useGetJobById({ initialData: initialJobData, jobId: params.jobId });
+  const loaderData = useLoaderData() as LoaderReturnType<typeof loader>;
+  const initialData = (loaderData as { initialJobData: JobDetails }).initialJobData;
+  const { data: jobData } = useGetJobById({ initialData, jobId: params.jobId });
   const job = jobData as JobDetails;
 
   return (
