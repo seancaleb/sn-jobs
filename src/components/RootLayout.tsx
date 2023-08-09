5c98a1869b8c3d82937976bb7f801412856fae26
Sean Caleb <scmubias@gmail.com>
2023-08-09 17:01:38 +0800

    refactor: refactor RootLayout component

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,24 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Outlet, useLocation, useMatch } from "react-router";
-import Header from "@/components/Header/Header";
+import { useLocation, useMatch } from "react-router";
 import TopLoadingBar from "@/components/TopLoadingBar";
-import Footer from "@/components/Footer/Footer";
 import { Toaster } from "@/components/ui/toaster";
-import { cn } from "@/lib/utils";
 import { useAppSelector } from "../app/hooks";
 import { selectNotification } from "@/features/notification/notificationSlice";
 import { useToast } from "@/components/ui/use-toast";
 import { useEffect } from "react";
 import ScrollToTop from "@/components/ScrollToTop";
+import UserLayout from "./UserLayout";
+import DashboardLayout from "./DashboardLayout";
 
 const RootLayout = () => {
   const location = useLocation();
   const { id } = useAppSelector(selectNotification);
   const { dismiss } = useToast();
   const matchers = useMatch("/jobs/:jobId");
-  const isSignInRoute = location.pathname === "/sign-in";
-  const isSignUpRoute = location.pathname === "/sign-up";
-  const isAuthRoute = isSignInRoute || isSignUpRoute;
+  const isEmployer = location.pathname.includes("employer");
+  const isAdmin = location.pathname.includes("admin");
+  const isEmployerOrAdmin = isEmployer || isAdmin;
 
   useEffect(() => {
     if (id) {
@@ -29,19 +28,10 @@ const RootLayout = () => {
 
   return (
     <>
-      {!isAuthRoute && <Header />}
-      <main
-        className={cn(
-          "relative min-h-screen container max-w-6xl",
-          !isAuthRoute && "pb-24 sm:pb-28"
-        )}
-      >
-        <ScrollToTop />
-        <TopLoadingBar />
-        <Toaster />
-        <Outlet />
-        {!isAuthRoute && <Footer />}
-      </main>
+      <ScrollToTop />
+      <TopLoadingBar />
+      <Toaster />
+      {isEmployerOrAdmin ? <DashboardLayout /> : <UserLayout />}
     </>
   );
 };
