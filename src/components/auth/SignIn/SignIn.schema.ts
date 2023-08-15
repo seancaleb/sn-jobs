8958354453e4a03bc3451cccc25af3455ab6f861
Sean Caleb <scmubias@gmail.com>
2023-08-15 23:25:14 +0800

    refactor: refactor schema error messages

--- src/components/auth/SignIn/SignIn.schema.ts
+++ src/components/auth/SignIn/SignIn.schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-  email: z.string().nonempty("Email is required").email("Please enter a valid email"),
-  password: z.string().nonempty("Password is required"),
+  email: z.string().nonempty("Please fill in your email.").email("Please enter a valid email."),
+  password: z.string().nonempty("Please fill in your password."),
 });
 
 export type SignInValues = z.infer<typeof signInSchema>;
