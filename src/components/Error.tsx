5a41e140a372ad0d0dddd23befe14770c5f2ad6d
Sean Caleb <scmubias@gmail.com>
2023-08-15 23:31:03 +0800

    refactor: refactor Error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,15 +1,16 @@
 import { useRouteError, isRouteErrorResponse, useNavigate } from "react-router-dom";
 import { isAPIResponseError } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { Balancer } from "react-wrap-balancer";
+import { AlertTriangle } from "lucide-react";
+import { Button } from "./ui/button";
+import { useDocumentTitle } from "@mantine/hooks";
 
 const ErrorRoute = () => {
   const error = useRouteError();
   let errorMessage: string;
   const navigate = useNavigate();
 
-  const handleNavigateBack = () => {
-    navigate(-1);
-  };
+  useDocumentTitle("Unexpected Error - SNJOBS");
 
   if (isRouteErrorResponse(error)) {
     // error is type `ErrorResponse`
@@ -19,19 +20,31 @@ const ErrorRoute = () => {
   } else if (typeof error === "string") {
     errorMessage = error;
   } else if (isAPIResponseError(error)) {
-    errorMessage = error.message;
+    errorMessage = error.message.split(":")[1];
+    console.log(errorMessage);
   } else {
     console.error(error);
     errorMessage = "Unknown error";
   }
 
   return (
-    <div id="error-page" className="section-padding text-center space-y-2">
-      <h1 className="text-4xl font-bold tracking-[-.5px]">Oops!</h1>
-      <p>{errorMessage}</p>
-      <Button variant="link" onClick={handleNavigateBack}>
-        Go Back
-      </Button>
+    <div className="bg-background fixed h-screen top-0 left-0 right-0 bottom-0 z-[5] grid place-items-center">
+      <div
+        id="error-page"
+        className="section-padding flex flex-col gap-4 items-center sm:items-start max-w-lg w-full px-5 text-center sm:text-left"
+      >
+        <AlertTriangle className="w-9 h-9 sm:w-10 sm:h-10 text-light" />
+
+        <h1 className="text-3xl sm:text-4xl font-bold">
+          <Balancer>Something went wrong</Balancer>
+        </h1>
+
+        <p>
+          <Balancer>{errorMessage}</Balancer>
+        </p>
+
+        <Button onClick={() => navigate(0)}>Retry</Button>
+      </div>
     </div>
   );
 };
