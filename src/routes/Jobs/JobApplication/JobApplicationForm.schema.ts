42b31e7cb24a56b15ec10572817376c457dceb1f
Sean Caleb <scmubias@gmail.com>
2023-08-16 00:15:08 +0800

    refactor: refactor JobApplicationForm component
    1. update schema error message
    2. minor style changes
    3. add Prompt component to handle pop up for unsaved changes when navigating to different routes

--- src/routes/Jobs/JobApplication/JobApplicationForm.schema.ts
+++ src/routes/Jobs/JobApplication/JobApplicationForm.schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const jobApplicationSchema = z.object({
-  resume: z.string().nonempty("Resume is required").url("Not a valid link"),
-  coverLetter: z.string().nonempty("Cover letter is required"),
+  resume: z.string().nonempty("Please fill in your resume.").url("Resume should be a valid link."),
+  coverLetter: z.string().nonempty("Please fill in your cover letter."),
 });
 
 export type JobApplicationValues = z.infer<typeof jobApplicationSchema>;
