2db169f61dac4999115d379ae2dd9fb223df7f81
Sean Caleb <scmubias@gmail.com>
2023-08-16 00:03:22 +0800

    style: minor style changes

--- src/routes/AccountSettingsRoute.tsx
+++ src/routes/AccountSettingsRoute.tsx
@@ -6,32 +6,29 @@ const links = [
   {
     label: "My Profile",
     path: "profile",
-    icon: <ProfileUser className="mr-3 h-5 w-5" strokeWidth={2.5} />,
+    icon: <ProfileUser className="h-4 w-4 mr-3" />,
   },
   {
     label: "Privacy & Security",
     path: "privacy-and-security",
-    icon: <Lock className="mr-3 h-5 w-5" strokeWidth={2.5} />,
+    icon: <Lock className="h-4 w-4 mr-3" />,
   },
 ];
 
 const AccountSettingsRoute = () => {
   return (
     <div className="py-8 space-y-6">
-      <div className="space-y-2">
-        <div className="text-3xl tracking-tight font-bold">Account Settings</div>
-        <p>Centralize your profile and settings.</p>
-      </div>
-
       <div className="flex gap-6 items-start">
-        <aside className="p-6 rounded-md border border-slate-200 max-w-[14em] w-full">
-          <ul className="space-y-6 text-[0.9375rem] font-medium">
+        <aside className="p-6 rounded-md border border-border max-w-[16rem] w-full hidden lg:block">
+          <ul className="space-y-1">
             {links.map(({ label, path, icon }) => (
               <li key={nanoid()}>
                 <NavLink
                   to={path}
                   className={({ isActive }) =>
-                    isActive ? "text-teal-600 flex items-center" : "flex items-center"
+                    `text-sm rounded-md px-4 py-2 h-10 flex items-center transition duration-150 hover:bg-accent hover:text-accent-foreground ${
+                      isActive ? "bg-accent text-accent-foreground" : "text-slate-500"
+                    }`
                   }
                 >
                   {icon}
@@ -42,7 +39,7 @@ const AccountSettingsRoute = () => {
           </ul>
         </aside>
 
-        <div className="p-6 rounded-md border border-slate-200 w-full">
+        <div className="lg:p-6 lg:rounded-md lg:border lg:border-border w-full">
           <Outlet />
         </div>
       </div>
