98e77230397ae0ebaec2e53d1c21b56316e03aa4
Sean Caleb <scmubias@gmail.com>
2023-08-16 00:12:04 +0800

    refactor: refactor schema error messages

--- src/routes/Users/Account/Security/DeleteProfile/DeleteProfile.schema.ts
+++ src/routes/Users/Account/Security/DeleteProfile/DeleteProfile.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const deleteProfileSchema = z.object({
-  password: z.string().nonempty("Please fill out this field"),
+  password: z.string().nonempty("Please fill in your password."),
 });
 
 export type DeleteProfileValues = z.infer<typeof deleteProfileSchema>;
