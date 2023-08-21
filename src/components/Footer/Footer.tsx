f327cd6adf49a772c67c5bc7c6e2fc53b9a8af78
Sean Caleb <scmubias@gmail.com>
2023-08-22 04:46:38 +0800

    refactor: update display text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,9 +3,7 @@ const Footer = () => {
     <footer className="py-6 border-t border-border absolute bottom-0 left-0 right-0 lg:left-5 lg:right-5">
       <div className="px-5 lg:px-0">
         <div className="flex flex-col sm:flex-row justify-between items-center space-y-1">
-          <span className="text-light text-xs text-center sm:text-left">
-            Copyright © 2023 SNJOBS. All rights reserved.
-          </span>
+          <span className="text-light text-xs text-center sm:text-left">© 2023 SNJOBS</span>
           <span className="text-light text-xs text-center sm:text-left">
             Developed by Sean Caleb
           </span>
