c6cec2bf23df8dfc49d454ca3e5a600f3acbadbd
Sean Caleb <scmubias@gmail.com>
2023-08-21 18:11:52 +0800

    refactor: refactor JobApplication page

--- src/routes/Jobs/JobApplication/JobApplication.page.tsx
+++ src/routes/Jobs/JobApplication/JobApplication.page.tsx
@@ -1,12 +1,10 @@
 /* eslint-disable react-refresh/only-export-components */
-import { fetchJobById, jobKeys, useGetJobById } from "@/api/jobs/jobs";
-import { ActionFunctionArgs, redirect, useLoaderData, useParams } from "react-router-dom";
+import { fetchJobById, jobKeys } from "@/api/jobs/jobs";
+import { ActionFunctionArgs, redirect } from "react-router-dom";
 import JobApplicationForm from "./JobApplicationForm";
 import { fetchUserProfile, userKeys } from "@/api/users/users";
 import { QueryClient } from "@tanstack/react-query";
 import store from "@/app/store";
-import { LoaderReturnType } from "@/types";
-import { JobDetails } from "@/api/jobs/jobs.type";
 
 export const loader =
   (queryClient: QueryClient) =>
@@ -32,21 +30,13 @@ export const loader =
       return redirect(`/jobs/${initialJobData.jobId}`);
     }
 
-    return {
-      initialJobData,
-    };
+    return null;
   };
 
 const JobApplication = () => {
-  const params = useParams<{ jobId: string }>();
-  const loaderData = useLoaderData() as LoaderReturnType<typeof loader>;
-  const initialData = (loaderData as { initialJobData: JobDetails }).initialJobData;
-  const { data: jobData } = useGetJobById({ initialData, jobId: params.jobId });
-  const job = jobData as JobDetails;
-
   return (
     <div className="w-full mb-4 sm:mb-0 lg:max-w-md">
-      <JobApplicationForm job={job} />
+      <JobApplicationForm />
     </div>
   );
 };
